a0e6b48bc1c2ce5923e951188c90298dc3255f19
rocduran <rocduran@gmail.com>
2015-10-02 19:15:52 +0200

    - Activity enquesta afegit - Canviat el logo de la app (launcher + notificacio + actionBar) @sesiomterim

---
+++ app/src/main/java/ad/uda/rocmoi/Enquesta.java
@@ -0,0 +1,13 @@
+package ad.uda.rocmoi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Enquesta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enquesta);
+    }
+}
