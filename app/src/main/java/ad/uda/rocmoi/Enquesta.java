8de50166d05814feb4181adb4010a42e68d03043
sesiomterim <sesiom_and@hotmail.com>
2015-10-06 13:26:39 +0200

    test commit

--- app/src/main/java/ad/uda/rocmoi/Enquesta.java
+++ app/src/main/java/ad/uda/rocmoi/Enquesta.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Enquesta extends AppCompatActivity {
-
+    //Comentari test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
