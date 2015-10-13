3962cf4a27356e6f5c51fb9d385fc5d127f81b1f
sesiomterim <sesiom_and@hotmail.com>
2015-10-13 22:16:34 +0200

    Creat package localBD + classes DBhelper i EnquestaREPO per a la gestio de la BD en local  (hi han tropecientos errors xq es un exemple que he pillat por ahi, pro no semble gaire xungo d'adaptar xD)

---
+++ app/src/main/java/ad/uda/rocmoi/localDB/DBhelper.java
@@ -0,0 +1,47 @@
+package ad.uda.rocmoi.localDB;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBhelper  extends SQLiteOpenHelper {
+    //version number to upgrade database version
+    //each time if you Add, Edit table, you need to change the
+    //version number.
+    private static final int DATABASE_VERSION = 4;
+
+    // Database Name
+    private static final String DATABASE_NAME = "enquestes.db";
+
+    public DBHelper(Context context ) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //All necessary tables you like to create will create here
+        //TODO crear les taules que farem servir per guardar totes les dades
+        //jo veig una taula enquestes (continuan amb la idea del ultim commit
+        //i una taula valoracions(del estil de la que tenim al server)
+        String CREATE_TABLE_STUDENT = "CREATE TABLE " + Student.TABLE  + "("
+                + Student.KEY_ID  + " INTEGER PRIMARY KEY AUTOINCREMENT ,"
+                + Student.KEY_name + " TEXT, "
+                + Student.KEY_age + " INTEGER, "
+                + Student.KEY_email + " TEXT )";
+
+        db.execSQL(CREATE_TABLE_STUDENT);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed, all data will be gone!!!
+        db.execSQL("DROP TABLE IF EXISTS " + Student.TABLE);
+
+        // Create tables again
+        onCreate(db);
+
+    }
+
+}
+}
